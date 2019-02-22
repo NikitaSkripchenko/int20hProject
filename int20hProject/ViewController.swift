75300b987a80b8456ad098069a32cdcea5e610ab
Nikita Skrypchenko <frickadelka456@gmail.com>
2019-02-22 13:51:40 +0200

    Initial Commit

---
+++ int20hProject/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  int20hProject
+//
+//  Created by Nikita Skrypchenko  on 2/22/19.
+//  Copyright © 2019 Nikita Skrypchenko . All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
