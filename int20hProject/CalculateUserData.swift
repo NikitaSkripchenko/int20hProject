7fc29d0ebb32b454507e7c6a17806ae753a774c6
Nikita Skrypchenko <frickadelka456@gmail.com>
2019-02-24 00:58:45 +0200

    add genre

--- int20hProject/CalculateUserData.swift
+++ int20hProject/CalculateUserData.swift
@@ -2,17 +2,16 @@
 import Foundation
 import UIKit
 
-class CalculateUserData: UIViewController{
-    override func viewDidLoad() {
-        super.viewDidLoad()
-       
-    }
+class CalculateUserData: UIViewController {
+    
+    
     @IBOutlet weak var userWeight: UITextField!
     
     @IBOutlet weak var userAge: UITextField!
     
-    @IBOutlet weak var userGender: UITextField!
+    @IBOutlet weak var genderPicker: UITextView!
     
+    @IBOutlet weak var userHeight: UITextField!
     
     @IBOutlet weak var proteinValue: UITextField!
     
@@ -33,5 +32,47 @@ class CalculateUserData: UIViewController{
     @IBAction func sendUserData(_ sender: UIButton) {
     }
     
+    let genreArray = ["Male", "Female"]
     
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        let pickerView = UIPickerView()
+        
+        pickerView.delegate = self
+        
+        genderPicker.inputView = pickerView
+    }
+    
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        
+        view.endEditing(true)
+    }
 }
+
+
+extension CalculateUserData: UIPickerViewDataSource {
+    
+    func numberOfComponents(in pickerView: UIPickerView) -> Int {
+        
+        return 1
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
+        
+        return genreArray.count
+    }
+}
+
+extension CalculateUserData: UIPickerViewDelegate {
+    
+    func pickerView(_ pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String? {
+        
+        return genreArray[row]
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, didSelectRow row: Int, inComponent component: Int) {
+        
+        genderPicker.text = genreArray[row]
+    }
+}
+
