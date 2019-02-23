e8fdca8bc982c0dca10ec40f2e8d33e2b5d155f2
IvanKarpets <ivankarpets@icloud.com>
2019-02-23 18:40:50 +0200

    add stuff

--- int20hProject/ViewController.swift
+++ int20hProject/ViewController.swift
@@ -10,11 +10,19 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBAction func signIn(_ sender: Any){
+        
+    }
+    
+    @IBOutlet weak var userName: UITextField!
+    
+    @IBOutlet weak var userPassword: UITextField!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
     }
-
-
+    
 }
 
