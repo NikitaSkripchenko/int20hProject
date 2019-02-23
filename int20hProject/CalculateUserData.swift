8909e86708962a7031a3e780b58272fa06aa6219
IvanKarpets <ivankarpets@icloud.com>
2019-02-23 20:46:12 +0200

    calculateUserData

---
+++ int20hProject/CalculateUserData.swift
@@ -0,0 +1,37 @@
+
+import Foundation
+import UIKit
+
+class CalculateUserData: UIViewController{
+    override func viewDidLoad() {
+        super.viewDidLoad()
+       
+    }
+    @IBOutlet weak var userWeight: UITextField!
+    
+    @IBOutlet weak var userAge: UITextField!
+    
+    @IBOutlet weak var userGender: UITextField!
+    
+    
+    @IBOutlet weak var proteinValue: UITextField!
+    
+    @IBOutlet weak var saltValue: UITextField!
+    
+    @IBOutlet weak var fatValue: UITextField!
+    
+    @IBOutlet weak var carbohydratesValue: UITextField!
+    
+    @IBOutlet weak var sugarValue: UITextField!
+    
+    @IBOutlet weak var kilocalories: UITextField!
+    
+    
+    @IBAction func calculateUserData(_ sender: UIButton) {
+        
+    }
+    @IBAction func sendUserData(_ sender: UIButton) {
+    }
+    
+    
+}
